70fb0d1e833c50ecb42b8050d3fa83b25546e230
Thomy <thomyfarhan@gmail.com>
2019-12-02 23:44:55 +0700

    [Fragment Title] Change layout to constraint and implement binding

--- app/src/main/java/com/aesthomic/explainimal/title/TitleFragment.kt
+++ app/src/main/java/com/aesthomic/explainimal/title/TitleFragment.kt
@@ -6,16 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.explainimal.R
+import com.aesthomic.explainimal.databinding.FragmentTitleBinding
 
 class TitleFragment : Fragment() {
+
+    private lateinit var binding: FragmentTitleBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_title, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_title, container, false)
+        return binding.root
     }
 
 
