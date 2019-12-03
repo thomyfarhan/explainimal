2dc94ea51f15119399268624143f4991bc1691dc
Thomy <thomyfarhan@gmail.com>
2019-12-04 02:36:41 +0700

    [Fragment Score] Init ViewModel and pass score value

--- app/src/main/java/com/aesthomic/explainimal/score/ScoreFragment.kt
+++ app/src/main/java/com/aesthomic/explainimal/score/ScoreFragment.kt
@@ -7,6 +7,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProviders
 
 import com.aesthomic.explainimal.R
 import com.aesthomic.explainimal.databinding.FragmentScoreBinding
@@ -14,6 +15,8 @@ import com.aesthomic.explainimal.databinding.FragmentScoreBinding
 class ScoreFragment : Fragment() {
 
     private lateinit var binding: FragmentScoreBinding
+    private lateinit var viewModel: ScoreViewModel
+    private lateinit var viewModelFactory: ScoreViewModelFactory
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -21,6 +24,14 @@ class ScoreFragment : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater,
             R.layout.fragment_score, container, false)
+
+        viewModelFactory = ScoreViewModelFactory(ScoreFragmentArgs.fromBundle(arguments!!).score)
+
+        viewModel = ViewModelProviders.of(this, viewModelFactory)
+            .get(ScoreViewModel::class.java)
+
+        binding.tvScoreScore.text = viewModel.score.toString()
+
         return binding.root
     }
     
