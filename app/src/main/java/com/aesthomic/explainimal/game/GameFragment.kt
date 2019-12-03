9dfe419945e0512774f14b1853575727ae89bf18
Thomy <thomyfarhan@gmail.com>
2019-12-03 16:39:00 +0700

    [Fragment Game] Change layout to constraint and implement binding

--- app/src/main/java/com/aesthomic/explainimal/game/GameFragment.kt
+++ app/src/main/java/com/aesthomic/explainimal/game/GameFragment.kt
@@ -6,17 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.explainimal.R
+import com.aesthomic.explainimal.databinding.FragmentGameBinding
 
 class GameFragment : Fragment() {
+
+    private lateinit var binding: FragmentGameBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_game, container, false)
+        return binding.root
     }
-
 
 }
