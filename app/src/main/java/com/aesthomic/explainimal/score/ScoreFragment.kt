8130f1eeea54a91e343567b9676923e3948cf417
Thomy <thomyfarhan@gmail.com>
2019-12-03 23:03:55 +0700

    [Fragment Score] Change layout to constraint and implement binding

--- app/src/main/java/com/aesthomic/explainimal/score/ScoreFragment.kt
+++ app/src/main/java/com/aesthomic/explainimal/score/ScoreFragment.kt
@@ -6,17 +6,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 
 import com.aesthomic.explainimal.R
+import com.aesthomic.explainimal.databinding.FragmentScoreBinding
 
 class ScoreFragment : Fragment() {
+
+    private lateinit var binding: FragmentScoreBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_score, container, false)
+        binding = DataBindingUtil.inflate(inflater,
+            R.layout.fragment_score, container, false)
+        return binding.root
     }
-
-
+    
 }
