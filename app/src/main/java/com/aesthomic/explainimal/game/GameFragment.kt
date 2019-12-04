041bc8b03dcaaa73c708a361cf13705295413636
Thomy <thomyfarhan@gmail.com>
2019-12-04 19:38:52 +0700

    [Fragment Game] Add buzz function

--- app/src/main/java/com/aesthomic/explainimal/game/GameFragment.kt
+++ app/src/main/java/com/aesthomic/explainimal/game/GameFragment.kt
@@ -1,11 +1,15 @@
 package com.aesthomic.explainimal.game
 
 
+import android.os.Build
 import android.os.Bundle
+import android.os.VibrationEffect
+import android.os.Vibrator
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.getSystemService
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
@@ -46,4 +50,16 @@ class GameFragment : Fragment() {
         viewModel.onGameFinishComplete()
     }
 
+    private fun buzz(pattern: LongArray) {
+        val buzzer = activity?.getSystemService<Vibrator>()
+        buzzer?.let {
+            // Vibrate for 500 milliseconds
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                buzzer.vibrate(VibrationEffect.createWaveform(pattern, -1))
+            } else {
+                //deprecated in API 26
+                buzzer.vibrate(pattern, -1)
+            }
+        }
+    }
 }
