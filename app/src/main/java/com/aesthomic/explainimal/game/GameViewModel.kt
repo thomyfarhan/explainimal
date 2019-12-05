ac31197d322be964a918747eee9e0c584cf65df4
Thomy <thomyfarhan@gmail.com>
2019-12-05 20:22:49 +0700

    [Fragment Game] Move Const Variable to companion object

--- app/src/main/java/com/aesthomic/explainimal/game/GameViewModel.kt
+++ app/src/main/java/com/aesthomic/explainimal/game/GameViewModel.kt
@@ -9,19 +9,21 @@ import androidx.lifecycle.ViewModel
 import com.aesthomic.explainimal.data.Animal
 import com.aesthomic.explainimal.data.AnimalData
 
-// Constant variables for time
-private const val DONE = 0L
-private const val ONE_SECOND = 1000L
-private const val COUNTDOWN_TIME = 60000L
-private const val COUNTDOWN_PANIC_TIME = 10000L
-
-// Constant variables for buzz
-private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
-private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
-private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
-private val NO_BUZZ_PATTERN = longArrayOf(0)
-
 class GameViewModel: ViewModel() {
+    companion object {
+        // Constant variables for time
+        private const val DONE = 0L
+        private const val ONE_SECOND = 1000L
+        private const val COUNTDOWN_TIME = 60000L
+        private const val COUNTDOWN_PANIC_TIME = 10000L
+
+        // Constant variables for buzz
+        private val CORRECT_BUZZ_PATTERN = longArrayOf(100, 100, 100, 100, 100, 100)
+        private val PANIC_BUZZ_PATTERN = longArrayOf(0, 200)
+        private val GAME_OVER_BUZZ_PATTERN = longArrayOf(0, 2000)
+        private val NO_BUZZ_PATTERN = longArrayOf(0)
+    }
+
     enum class BuzzType(val pattern: LongArray) {
         CORRECT(CORRECT_BUZZ_PATTERN),
         GAME_OVER(GAME_OVER_BUZZ_PATTERN),
